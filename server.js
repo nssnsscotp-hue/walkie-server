9ed058d80cf06a0754a7a2ac6e1db1e1b0cea43a
nssnsscotp-hue <nssnsscotp@gmail.com>
2026-04-03 20:05:03 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const { Server } = require("socket.io");
 
 const app = express();
 const server = http.createServer(app);
-
 const io = new Server(server, {
   cors: { origin: "*" }
 });
@@ -12,24 +11,22 @@ const io = new Server(server, {
 io.on("connection", (socket) => {
   console.log("User connected:", socket.id);
 
+  // Join channel
   socket.on("join-channel", (channel) => {
+    console.log("Joined:", channel);
     socket.join(channel);
-    socket.join("global");
   });
 
-  socket.on("audio-stream", ({ channel, audio }) => {
-    socket.to(channel).emit("audio-stream", {
-      type: "normal",
-      audio
-    });
+  // 🎤 Live audio stream
+  socket.on("audio-stream", (data) => {
+    socket.to(data.channel).emit("audio-stream", data.audio);
   });
 
+  // 🚨 Emergency (send to all)
   socket.on("emergency-stream", (audio) => {
-    io.emit("audio-stream", {
-      type: "emergency",
-      audio
-    });
+    io.emit("audio-stream", audio);
   });
+
 });
 
 const PORT = process.env.PORT || 3000;
